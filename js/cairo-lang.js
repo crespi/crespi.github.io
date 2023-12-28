99ba8b8933d381eb49b78fd80c4b5a40c998195c
Francisco Crespi <crespi.f@gmail.com>
2023-12-28 12:10:59 +0100

    cairo tests js

---
+++ js/cairo-lang.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/Yj1wRgfB5nhWyg3c/scene.splinecode');
